70a5d1e94ccdd8946cc2d3829bafebc574d78491
GChukwudi <g.chukwudi@alustudent.com>
2024-02-06 08:29:18 +0200

    updated

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ let qrInput = document.getElementById('qrInput');
     
 function generateQRCode() {
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrInput.value;
+
+    qr_code.classList.add('show-img');
 }
