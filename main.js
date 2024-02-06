3c6c99161f7c44005af384f592aec09e0306b226
GChukwudi <g.chukwudi@alustudent.com>
2024-02-06 08:50:01 +0200

    qr code generator created

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ let qrImage = document.getElementById('qrImage');
 let qrInput = document.getElementById('qrInput');
     
 function generateQRCode() {
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrInput.value;
+    if(qrInput.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrInput.value;
 
-    qr_code.classList.add('show-img');
+        qr_code.classList.add('show-img');
+    }else{
+        qrInput.classList.add('error');
+        setTimeout(() => {
+            qrInput.classList.remove('error');
+        }, 1000);
+    }
 }
